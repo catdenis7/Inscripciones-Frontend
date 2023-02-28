6399a9363dd64e5fb8cb5ed2ef0cc0d99a58cd57
Daniel Cueto <danielcuetorrico@gmail.com>
2023-02-28 15:21:56 -0400

    mas problemas

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
     return (
         <>
     <Box Col='3' className='home-box'>
-         <CardIcon  />
+         <CardIcon icono={levantamiento}  />
          <CardIcon />
          <CardIcon />
         
